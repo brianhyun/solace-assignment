0249691d29732ea0f3d0b3e11c38a42a11a99cb8
Brian Hyun <brianhyun@protonmail.com>
2024-10-12 13:50:54 -0700

    Update title font styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
 
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-10">
-      <h1>Solace Advocates</h1>
+      <h1 className="text-2xl font-medium text-gray-800">Solace Advocates</h1>
       <br />
       <div className="flex items-stretch gap-2 mt-2">
         <Input onChange={onChange} />
